09680eb83206e25d43dd7041f068fe2a17cd0609
Hoon9901 <shonn.dev@gmail.com>
2022-01-12 19:26:05 +0900

    4장

---
+++ src/main/java/com/example/jwttutorial/dto/UserDto.java
@@ -0,0 +1,30 @@
+package com.example.jwttutorial.dto;
+
+import com.example.jwttutorial.entity.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.validation.constraints.*;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+
+}
